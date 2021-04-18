02565c42dc58b264705ea074785149def5570589
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2021-04-18 16:54:25 +0700

    move App to component folder

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import SimpleStorageContract from "../../contracts/SimpleStorage.json";
+import getWeb3 from "../../getWeb3";
 
 import "./App.css";
 
