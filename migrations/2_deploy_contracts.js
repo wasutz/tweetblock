12ae11786d762fd3d87a5d446ce98bfdd703c089
Pao - Wasut Panyawiphat <wasut_pao@hotmail.com>
2021-04-19 01:49:25 +0700

    remove unused contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var TweetManager = artifacts.require("./TweetManager.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(TweetManager);
 };
